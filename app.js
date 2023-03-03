6c873079b3a89d4a0f6222eb9927ebae977524e9
Govind Kumar <gk4051668@gmail.com>
2023-03-03 20:39:48 +0530

    added cors

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 app.use("/diary", DiaryRouter);
 
