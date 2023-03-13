b3099f67fa131fe1328f4fc744526c7481ee0df4
Govind Kumar <gk4051668@gmail.com>
2023-03-14 00:19:56 +0530

    added cake data

---
+++ models/Cake.js
@@ -0,0 +1,22 @@
+import { getDb } from "../config/db.js";
+
+export class Cake {
+  constructor() {}
+  static getAllCakes() {
+    const db = getDb();
+    return db
+      .collection("cakes")
+      .find()
+      .toArray()
+      .then((cakes) => cakes)
+      .catch((error) => error);
+  }
+  static addCake(data) {
+    const db = getDb();
+    return db
+      .collection("cakes")
+      .insertOne(data)
+      .then((cake) => cake)
+      .catch((error) => error);
+  }
+}
